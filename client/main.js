9af65bede0d26ab595038b283f489dfa21500e4d
kenji <indytrading@gmail.com>
2015-11-07 23:03:54 +0700

    The Rocket Shop
    The Rocket Shop

---
+++ client/main.js
@@ -0,0 +1,10 @@
+UI.registerHelper("money", function(amout){
+  return accounting.formatMoney(amout);
+
+});
+
+UI.registerHelper("markdown", function(text){
+  var converter = new show.down.Converter;
+  return accounting.formatMoney(amout);
+
+});
